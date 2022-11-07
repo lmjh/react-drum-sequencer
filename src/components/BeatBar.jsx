1ad9077c9e6418710482432466c44ec4220eb7f8
lmjh <lmjhemming@gmail.com>
2022-11-07 11:59:52 +0000

    Set up BeatBar components to track current beat

--- src/components/BeatBar.jsx
+++ src/components/BeatBar.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { BeatLight } from './';
 
+/**
+ * Builds and displays the array of BeatLight components to track the current beat
+ */
 const BeatBar = () => {
+  const beatArray = new Array(16).fill("").map((_ele, index) => <BeatLight key={index} beatLightNum={index} />)
+
   return (
     <div>
         <div>BeatBar</div>
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
-        <BeatLight />
+        { beatArray }
     </div>
   )
 }
