25660b66df019625a80be61a6fc632d9a2c928cc
lmjh <lmjhemming@gmail.com>
2022-11-08 15:06:21 +0000

    Display current beat on TrackToggles

--- src/components/TrackToggle.jsx
+++ src/components/TrackToggle.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 
+import { BeatContext } from "../contexts";
+
 const TrackToggle = ({ index, active, toggleFunction }) => {
+    const beat = useContext(BeatContext);
+    
     return (
         <button onClick={() => toggleFunction(index)}>
-            {active ? "[*]" : "[_]"}
+            {index === beat.beat ? "[0]" : active ? "[*]" : "[_]"}
         </button>
     );
 };
