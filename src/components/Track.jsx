ef042c95423d4914cad037305de89bd114793212
lmjh <lmjhemming@gmail.com>
2022-11-08 11:24:39 +0000

    Add state and context variables to Track

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useRef, useContext } from "react";
+
 import { TrackControlPanel, TrackBar } from "./";
+import { SettingsContext } from "../contexts";
 
 const Track = () => {
+    const [trackIsMuted, setTrackIsMuted] = useState(false);
+    const [trackVolume, setTrackVolume] = useState(0.6);
+    const [trackPattern, setTrackPattern] = useState([
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    ]);
+
+    const { globalVolume, globalIsMuted } = useContext(SettingsContext);
+
     return (
         <div>
             <div>Track</div>
