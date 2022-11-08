5d627d9238a05f7fbd43cbd08ae46b470fa3aebe
lmjh <lmjhemming@gmail.com>
2022-11-08 14:10:48 +0000

    Add prop types to TrackControlPanel

--- src/components/TrackControlPanel.jsx
+++ src/components/TrackControlPanel.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import { VolumeControl, ToggleButton } from "./";
 
 const TrackControlPanel = ({
@@ -19,6 +21,13 @@ const TrackControlPanel = ({
             <VolumeControl volume={trackVolume} setVolume={setTrackVolume} />
         </div>
     );
+};
+
+TrackControlPanel.propTypes = {
+    isTrackMuted: PropTypes.bool.isRequired,
+    toggleTrackMute: PropTypes.func.isRequired,
+    trackVolume: PropTypes.number.isRequired,
+    setTrackVolume: PropTypes.func.isRequired,
 };
 
 export default TrackControlPanel;
