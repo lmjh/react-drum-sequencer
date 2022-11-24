0ad92685a97f8f7bbac8ac43096e4aac842372cc
lmjh <lmjhemming@gmail.com>
2022-11-24 11:42:13 +0000

    Add drum samples

---
+++ src/audio/index.js
@@ -0,0 +1,15 @@
+import { default as floortom } from "./floortom.wav";
+import { default as hihat } from "./hihat.wav";
+import { default as snare } from "./snare.wav";
+import { default as bass } from "./bass.wav";
+
+
+// samples sourced from https://www.indiedrums.com/2012/10/10/free-drums-samples-rock-kit/
+const samples = [
+    { name: "Floor Tom", sample: floortom },
+    { name: "Hi-hat", sample: hihat },
+    { name: "Snare", sample: snare },
+    { name: "Bass", sample: bass },
+];
+
+export default samples;
