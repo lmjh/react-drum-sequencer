ed33be35a06b478480d2df8ef41d893b006c16bc
lmjh <lmjhemming@gmail.com>
2022-11-26 14:07:35 +0000

    Memoise TempoControl component and functions

--- src/components/TempoControl.jsx
+++ src/components/TempoControl.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useCallback } from "react";
 import { SettingsContext } from "../contexts";
 import ActionButton from "./ActionButton";
 
@@ -10,53 +10,72 @@ const TempoControl = () => {
 
     const [displayTempo, setDisplayTempo] = useState(tempo.current);
 
-    const increment = (step) => {
-        tempo.current + step < 201
-            ? (tempo.current += step)
-            : (tempo.current = 200);
-        setDisplayTempo(tempo.current);
-    };
-
-    const decrement = (step) => {
-        tempo.current - step > 39
-            ? (tempo.current -= step)
-            : (tempo.current = 40);
-        setDisplayTempo(tempo.current);
-    };
+    const increment = useCallback(
+        (step) => {
+            tempo.current + step < 201
+                ? (tempo.current += step)
+                : (tempo.current = 200);
+            setDisplayTempo(tempo.current);
+        },
+        [tempo]
+    );
+    const decrement = useCallback(
+        (step) => {
+            tempo.current - step > 39
+                ? (tempo.current -= step)
+                : (tempo.current = 40);
+            setDisplayTempo(tempo.current);
+        },
+        [tempo]
+    );
 
     return (
         <div className="tempoControl">
             <div className="tempoDisplay">
-                <div className="tempoDisplayInner">
-                    {displayTempo}
-                </div>
+                <div className="tempoDisplayInner">{displayTempo}</div>
             </div>
             <ActionButton
                 label="<<"
-                actionFunction={() => decrement(10)}
+                actionFunction={useCallback(() => decrement(10), [])}
                 className="tempoButton"
-                innerClassName={tempo.current > 40 ? "tempoButtonInner" : "tempoButtonInnerInactive"}
+                innerClassName={
+                    tempo.current > 40
+                        ? "tempoButtonInner"
+                        : "tempoButtonInnerInactive"
+                }
             />
             <ActionButton
                 label="<"
-                actionFunction={() => decrement(1)}
+                actionFunction={useCallback(() => decrement(1), [])}
                 className="tempoButton"
-                innerClassName={tempo.current > 40 ? "tempoButtonInner" : "tempoButtonInnerInactive"}
+                innerClassName={
+                    tempo.current > 40
+                        ? "tempoButtonInner"
+                        : "tempoButtonInnerInactive"
+                }
             />
             <ActionButton
                 label=">"
-                actionFunction={() => increment(1)}
+                actionFunction={useCallback(() => increment(1), [])}
                 className="tempoButton"
-                innerClassName={tempo.current < 200 ? "tempoButtonInner" : "tempoButtonInnerInactive"}
+                innerClassName={
+                    tempo.current < 200
+                        ? "tempoButtonInner"
+                        : "tempoButtonInnerInactive"
+                }
             />
             <ActionButton
                 label=">>"
-                actionFunction={() => increment(10)}
+                actionFunction={useCallback(() => increment(10), [])}
                 className="tempoButton"
-                innerClassName={tempo.current < 200 ? "tempoButtonInner" : "tempoButtonInnerInactive"}
+                innerClassName={
+                    tempo.current < 200
+                        ? "tempoButtonInner"
+                        : "tempoButtonInnerInactive"
+                }
             />
         </div>
     );
 };
 
-export default TempoControl;
+export default React.memo(TempoControl);
