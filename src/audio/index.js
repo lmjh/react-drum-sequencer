7ec11fbf005d0df045ab17fd1404ede654aa0d68
lmjh <lmjhemming@gmail.com>
2023-01-10 11:16:07 +0000

    Update track order

--- src/audio/index.js
+++ src/audio/index.js
@@ -6,10 +6,10 @@ import { default as bass } from "./bass.wav";
 
 // samples sourced from https://www.indiedrums.com/2012/10/10/free-drums-samples-rock-kit/
 const samples = [
-    { name: "Floor Tom", sample: floortom },
-    { name: "Hi-hat", sample: hihat },
-    { name: "Snare", sample: snare },
     { name: "Bass", sample: bass },
+    { name: "Snare", sample: snare },
+    { name: "Hi-hat", sample: hihat },
+    { name: "Floor Tom", sample: floortom },
 ];
 
 export default samples;
