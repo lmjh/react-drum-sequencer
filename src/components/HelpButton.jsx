6ca6fcc9723ec52c4e6066713ee0b2a86cbe2b2b
lmjh <lmjhemming@gmail.com>
2023-01-20 11:33:18 +0000

    Build and style HelpButton

--- src/components/HelpButton.jsx
+++ src/components/HelpButton.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const HelpButton = () => {
-    return <div>
-        
-    </div>;
+    return (
+        <button className="helpButton" aria-label="Open help modal">
+            <div className="helpButtonInner">
+                <FontAwesomeIcon icon="fa-regular fa-circle-question" />
+            </div>
+        </button>
+    );
 };
 
 export default HelpButton;
